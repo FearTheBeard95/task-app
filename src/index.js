79db394e822219bc669d5900ac0a91b43a0e4539
Carlos Sibalatani <carlos.sibalatani@mhss.gov.na>
2021-04-19 22:15:33 +0200

    Add update HTTP endpoints

--- src/index.js
+++ src/index.js
@@ -41,6 +41,25 @@ app.get('/users/:id', async (req, res)=>{
     }
 })
 
+app.patch('/users/:id', async(req,res)=>{
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if(!isValidOperation){
+        return res.status(400).send({error: 'Invalid updates'})
+    }
+    try {
+        const user = await users.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
 app.post('/tasks', async (req, res)=>{
     const task = new tasks(req.body)
 
@@ -68,6 +87,25 @@ app.get('/tasks/:id', async (req, res)=>{
         res.status(302).send(task)
     } catch (error) {
         res.status(404).send(error)
+    }
+})
+
+app.patch('/tasks/:id', async (req, res)=>{
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if(!isValidOperation){
+        return res.status(400).send({error: 'Invalid updates'})
+    }
+    try {
+        const task = await tasks.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (error) {
+        res.status(500).send()
     }
 })
 
