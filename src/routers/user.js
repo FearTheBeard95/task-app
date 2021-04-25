80ee91496e1bb4fd17053aa8caaee1fa47a92462
Carlos Sibalatani <carlos.sibalatani@mhss.gov.na>
2021-04-25 23:35:13 +0200

    Add delete avatar

--- src/routers/user.js
+++ src/routers/user.js
@@ -87,7 +87,6 @@ router.delete('/users/me', auth, async(req, res)=>{
 })
 
 const upload = multer({
-    dest: 'avatar',
     limits: {
         fileSize: 1000000
     },
@@ -101,11 +100,19 @@ const upload = multer({
 
 router.post('/users/me/avatar', auth, upload.single('avatar') , async(req, res)=>{
     try {
+        req.user.avatar = req.file.buffer
+        await req.user.save()
         res.send()
     } catch (error) {
         res.status(500).send(error)
     }
 }, (error, req, res, next) =>{
     res.status(400).send({error: error.message})
+})
+
+router.delete('/users/me/avatar', auth, async(req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
 })
 module.exports = router
