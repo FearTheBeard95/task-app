deffa792f248458932abf6edcf3745cd9a03ec8e
Carlos Sibalatani <carlos.sibalatani@mhss.gov.na>
2021-04-27 13:25:28 +0200

    Add file upload feature

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const taskRouter = require('./routers/task')
 const multer = require('multer')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 app.use(express.json())
 app.use(userRouter)
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,7 @@ const {users} = require('../models/user')
 const auth = async (req, res, next) =>{
     try {
         const token = req.header('Authorization').replace('Bearer ','')
-        const decoded = jwt.verify(token, 'thisisanexample')
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
         const user = await users.findOne({_id: decoded._id, 'tokens.token': token})
 
